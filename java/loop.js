5196efe2fe3059e6c0ed009fd038ac435c5f8053
EclipseGarden <vivian.fryer02@gmail.com>
2023-05-18 11:25:42 -0400

    java

---
+++ java/loop.js
@@ -0,0 +1,12 @@
+for(let i=1;i<20;i++){
+    console.log ("Nadia")
+}
+for(let i=1;i<=20;i++){
+    console.log("ottawa")
+}
+for(let i=0; i<8;i++){
+    console.log(i)
+}
+for(let i=2;i<=20;i+=2){
+    console.log(i)
+}
