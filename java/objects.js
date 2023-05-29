e3281b961e8b54e44138c6a2b2a57bae374b5b40
EclipseGarden <vivian.fryer02@gmail.com>
2023-05-29 12:20:01 -0400

    DOM

---
+++ java/objects.js
@@ -0,0 +1,11 @@
+const user={
+    fisrtName:'nadia',
+    lastname:'amin',
+    city:'ottawa',
+    marks:'88'
+}
+
+console.log("my first name is",user.firstname);
+console.log("my last name is", user.lastname);
+console.log("I reside in",user.city);
+
